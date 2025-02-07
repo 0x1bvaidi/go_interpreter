e9bd3cc0ef46f737f42eb6db57145c164ea27734
0x1bvaidi <vaidityachouhan@gmail.com>
2025-02-07 21:39:46 +0530

    Contain lexer implementation of letters and number

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -29,6 +29,7 @@ func (l *Lexer) readChar() {
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -50,9 +51,51 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookUpIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	Position := l.Position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[Position: l.Position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && 'Z' <= ch || ch == '_'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	Position := l.Position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[Position:l.Position]
+}
+
+func isDigit(ch byte) bool{
+	return '0' <= ch && ch<= '9'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
