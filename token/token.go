92cc13131e480803364e96b0f84f366dc74bc281
0x1bvaidi <vaidityachouhan@gmail.com>
2025-04-03 01:54:42 +0530

    All the tokens for the basic interpreter

---
+++ token/token.go
@@ -0,0 +1,38 @@
+package token
+
+type TokenType string
+
+// type = "string which is the value of the token of that type"
+type token struct {
+	Type  TokenType
+	Literal string
+}
+
+const(
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// literals and identifiers
+	IDENT = "IDENT"
+	INT = "INT"
+
+	//delimiters
+	COMMENT = "$"
+	STOP = "@"
+	RBRACE = "{"
+	LBRACE = "}"
+	RPAREN = "("
+	LPAREN = ")"
+	
+	//Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+	RETURN = "RETURN"
+
+	//operators
+	ASSIGN = "="
+	MULT = "*"
+	ADD = "+"
+	MINUS = "-"
+	DIVIDE = "/"
+)
