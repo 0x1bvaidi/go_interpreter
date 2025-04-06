a053bb59f201849c52d6387161049803d2372800
0x1bvaidi <vaidityachouhan@gmail.com>
2025-04-06 23:40:39 +0530

    Add function

--- token/token.go
+++ token/token.go
@@ -37,3 +37,18 @@ const (
 	MINUS  = "-"
 	DIVIDE = "/"
 )
+
+var keywords = map[string]TokenType{
+	"phi": FUNCTION,
+	"let": LET,
+	"return": RETURN,
+}
+
+//  lookupIdent checks the keywords table to see whether the given identifier is in fact a keyword.
+//  If it is, it returns the keyword’s TokenType constant. If it isn’t, we just get back token.IDENT
+func LookUpIdent(ident string) TokenType{
+	if tok, ok := keywords[ident]; ok{
+		return tok
+	}
+	return IDENT
+}
